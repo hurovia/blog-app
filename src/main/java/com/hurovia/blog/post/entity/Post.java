00463d5e0213a2b19bd6a536775bbc3f0dc3df59
hurovia <hurovia@gmail.com>
2026-09-11 10:53:09 +0530

    Updated entity Post

--- src/main/java/com/hurovia/blog/post/entity/Post.java
+++ src/main/java/com/hurovia/blog/post/entity/Post.java
@@ -1,18 +1,48 @@
 package com.hurovia.blog.post.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
 
 @Entity
+@Table(name = "posts")
 public class Post {
+    public Long getPostId() {
+        return postId;
+    }
+
+    public void setPostId(Long postId) {
+        this.postId = postId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Column(name = "post_id", updatable = false, nullable = false)
+    private Long postId;
+
+    @Column(name = "post_title", nullable = false)
     private String title;
-    private String description;
-    private LocalDateTime timestamp;
+
+    @Lob
+    @Column(name = "post_content", columnDefinition = "MEDIUMTEXT")
+    private String content;
+
+    @Column(name = "time_posted", updatable = false, nullable = false)
+    private Instant createdDate;
 }

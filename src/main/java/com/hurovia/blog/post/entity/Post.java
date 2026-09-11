be01d201ac3447a4de51aada6cc2a52673a16d2c
hurovia <hurovia@gmail.com>
2026-09-11 07:32:40 +0530

    added JPA and made some changes to entity Post class

--- src/main/java/com/hurovia/blog/post/entity/Post.java
+++ src/main/java/com/hurovia/blog/post/entity/Post.java
@@ -1,4 +1,18 @@
 package com.hurovia.blog.post.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private LocalDateTime timestamp;
 }
